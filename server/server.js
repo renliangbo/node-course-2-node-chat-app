4de4d331522ea8446613d58d421cbca045dd89c0
renliangbo <renna0508@126.com>
2017-12-06 08:48:50 +0800

    git init

---
+++ server/server.js
@@ -0,0 +1,14 @@
+//binconsole.log(__dirname + '/../public')
+const path = require('path');
+const express = require('express')
+var publicPath = path.join(__dirname, '../public');
+
+const port = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+		console.log(`App start on port ${port}`)
+})
