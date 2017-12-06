7b305b0b83a24a6078fb1d08aff4832ae8e76908
renliangbo <renna0508@126.com>
2017-12-06 21:26:40 +0800

    add connect and disconect

--- server/server.js
+++ server/server.js
@@ -1,14 +1,24 @@
 //binconsole.log(__dirname + '/../public')
 const path = require('path');
-const express = require('express')
+const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http')
 var publicPath = path.join(__dirname, '../public');
 
 const port = process.env.PORT || 3000;
 
 var app = express();
 
+var server = http.createServer(app);
+var io = socketIO(server)
 app.use(express.static(publicPath));
+io.on('connection', (socket) => {
+		console.log('new user connection');
+		socket.on('disconnect', () => {
+				console.log('user disconnect')
+		})
+})
 
-app.listen(port, () => {
+server.listen(port, () => {
 		console.log(`App start on port ${port}`)
 })
